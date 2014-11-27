2b3b9e9b6b93a614dff236eab4f7cb9b33acb763
sachin <goyalsachin52@gmail.com>
2014-11-27 15:16:44 +0530

    first commit

---
+++ app/assets/javascripts/angular/controllers/stocks_ctrl.js
@@ -0,0 +1,17 @@
+
+app.controller('StocksCtrl', ['$scope', 'Stock', '$filter', function($scope, Stock, $filter) {
+  $scope.stocks = Stock.all();
+
+  $scope.createStock = function() {
+    var attr = {};
+    attr.symbol = $filter('uppercase')($scope.newSymbol);
+    var newStock = Stock.create(attr);
+    $scope.stocks.push(newStock);
+    $scope.newSymbol = "";
+  };
+
+  $scope.deleteStock = function(id, idx) {
+    Stock.delete(id);
+    $scope.stocks.splice(idx, 1);
+  };
+}]);
